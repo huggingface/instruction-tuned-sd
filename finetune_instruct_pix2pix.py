dd5c1bc1f16850361f6c3db0085de6371c5ffe34
Sayak Paul <spsayakpaul@gmail.com>
2023-03-17 15:32:47 +0530

    change: tracker name.

--- finetune_instruct_pix2pix.py
+++ finetune_instruct_pix2pix.py
@@ -856,7 +856,7 @@ def main():
     # We need to initialize the trackers we use, and also store our configuration.
     # The trackers initializes automatically on the main process.
     if accelerator.is_main_process:
-        accelerator.init_trackers("instruct-pix2pix", config=vars(args))
+        accelerator.init_trackers("instruct-pix2pix-cartoonizer", config=vars(args))
 
     # Train!
     total_batch_size = (
