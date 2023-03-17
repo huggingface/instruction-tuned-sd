6ec8eb111dfaf903df16a088e66ca03360c28ada
Sayak Paul <spsayakpaul@gmail.com>
2023-03-17 14:34:52 +0530

    export to hub script.

--- data_preparation/generate_dataset.py
+++ data_preparation/generate_dataset.py
@@ -16,10 +16,8 @@ def parse_args():
         "--model_id", type=str, default="sayakpaul/whitebox-cartoonizer"
     )
     parser.add_argument("--dataset_id", type=str, default="imagenette")
-    # parser.add_argument("--instructions_path", type=str, default="instructions.txt")
     parser.add_argument("--max_num_samples", type=int, default=5000)
     parser.add_argument("--data_root", type=str, default="cartoonizer-dataset")
-    # parser.add_argument("--push_to_hub", action="store_true")
     args = parser.parse_args()
     return args
 
@@ -33,38 +31,11 @@ def load_dataset(dataset_id, max_num_samples):
     return dataset
 
 
-# def load_instructions(instructions_path: str) -> List[str]:
-#     with open(instructions_path, "r") as f:
-#         instructions = f.readlines()
-#     instructions = [i.strip() for i in instructions]
-#     return instructions
-
-
-# def gen_examples(
-#     dataset: tf.data.Dataset, instructions: List[str], concrete_fn: Callable
-# ):
-#     def fn():
-#         for sample in dataset.as_numpy_iterator():
-#             original_image = sample["image"]
-#             cartoonized_image = model_utils.perform_inference(
-#                 concrete_fn, original_image
-#             )
-#             yield {
-#                 "original_image": original_image,
-#                 "edit_prompt": np.random.choice(instructions),
-#                 "cartoonized_image": cartoonized_image,
-#             }
-
-#     return fn
-
-
 def main(args):
     print("Loading initial dataset and the Cartoonizer model...")
     dataset = load_dataset(args.dataset_id, args.max_num_samples)
-    # instructions = load_instructions(args.instructions_path)
     concrete_fn = model_utils.load_model(args.model_id)
     inference_fn = model_utils.perform_inference(concrete_fn)
-    # generator_fn = gen_examples(dataset, instructions, concrete_fn)
 
     print("Preparing the image pairs...")
     os.makedirs(args.data_root, exist_ok=True)
@@ -81,23 +52,6 @@ def main(args):
         cartoonized_image.save(os.path.join(sample_dir, "cartoonized_image.png"))
 
     print(f"Total generated image-pairs: {len(os.listdir(args.data_root))}.")
-    # print("Creating dataset...")
-    # hub_ds = Dataset.from_generator(
-    #     generator_fn,
-    #     features=Features(
-    #         original_image=ImageFeature(),
-    #         edit_prompt=Value("string"),
-    #         cartoonized_image=ImageFeature(),
-    #     ),
-    # )
-
-    # if args.push_to_hub:
-    #     print("Pushing to the Hub...")
-    #     ds_name = f"cartoonizer-dataset"
-    #     if args.max_num_samples is not None:
-    #         num_samples = args.max_num_samples
-    #         ds_name += f"{num_samples}-samples"
-    #     hub_ds.push_to_hub(ds_name)
 
 
 if __name__ == "__main__":
