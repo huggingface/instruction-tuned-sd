9c2cdb9c24b40ccee2e6080bcfcf432ea4d8e505
Sayak Paul <spsayakpaul@gmail.com>
2023-03-18 09:59:53 +0530

    add: main

--- cartoonize.py
+++ cartoonize.py
@@ -2,6 +2,7 @@ import argparse
 import torch
 from diffusers import StableDiffusionInstructPix2PixPipeline
 from diffusers.utils import load_image
+import os
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -12,7 +13,7 @@ def parse_args():
     parser.add_argument("--prompt", type=str, default="Generate a cartoonized version of the image")
     parser.add_argument("--num_inference_steps", type=int, default=20)
     parser.add_argument("--image_guidance_scale", type=float, default=1.5)
-    parser.add_argument("--image_guidance_scale", type=float, default=7.)
+    parser.add_argument("--guidance_scale", type=float, default=7.)
     parser.add_argument("--num_images_per_prompt", type=float, default=3)
     args = parser.parse_args()
     return args
@@ -23,15 +24,17 @@ def load_pipeline(model_id):
     return pipeline
 
 def main(args):
+    os.makedirs(args.model_id, exist_ok=True)
     pipeline = load_pipeline(args.model_id)
     image = load_image(args.image_path)
     
     print("Generating images...")
     images = pipeline(args.prompt, image=image, num_inference_steps=args.num_inference_steps, image_guidance_scale=args.image_guidance_scale, guidance_scale=args.guidance_scale, num_images_per_prompt=args.num_images_per_prompt).images
     image_prefix = f"steps@{args.num_inference_steps}-igs@{args.image_guidance_scale}-gs@{args.guidance_scale}"
+    os.makedirs(os.path.join(args.args.model_id, image_prefix), exist_ok=True)
     for i, image in enumerate(images): 
-        image_filename = image_prefix + f"_{i}.png"
-        image.save(image_filename)
+        image_path = os.path.join(args.args.model_id, image_prefix, f"_{i}.png")
+        image.save(image_path)
 
 
 if __name__ == "__main__":
