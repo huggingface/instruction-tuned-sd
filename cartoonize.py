004291d31ae77560ac5900e5a8e32022f0ecd9f9
Sayak Paul <spsayakpaul@gmail.com>
2023-03-18 10:16:09 +0530

    more refactoring.

--- cartoonize.py
+++ cartoonize.py
@@ -58,10 +58,10 @@ def main(args):
 
     print(f"Serializing images to {os.path.join(args.model_id, args.concept)}...")
     image_prefix = f"steps@{args.num_inference_steps}-igs@{args.image_guidance_scale}-gs@{args.guidance_scale}"
-    os.makedirs(os.path.join(args.model_id, image_prefix), exist_ok=True)
+    os.makedirs(os.path.join(args.model_id, args.concept, image_prefix), exist_ok=True)
     for i, image in enumerate(images):
         image_path = os.path.join(
-            args.model_id, args.concept, image_prefix, f"_{i}.png"
+            args.model_id, args.concept, image_prefix, f"{args.concept}_{i}.png"
         )
         image.save(image_path)
 
